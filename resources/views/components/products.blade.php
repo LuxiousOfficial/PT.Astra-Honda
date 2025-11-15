6873089d70ba806632cc156c3361da8d57090e64
Rommy Ardiansyah <rommyardiansyah09@gmail.com>
2025-11-15 20:24:38 +0700

    Memperbarui tombol see more pada products

--- resources/views/components/products.blade.php
+++ resources/views/components/products.blade.php
@@ -17,9 +17,4 @@
             {{ $slot }}
         </div>
     </div>
-    <div class="row">
-      <div class="col text-center mt-5 text-capitalize">
-        <a href="/products" class="fs-5 fw-bold text-decoration-none p-3 bg-danger text-light rounded shadow">see more</a>
-      </div>
-    </div>
   </div>
